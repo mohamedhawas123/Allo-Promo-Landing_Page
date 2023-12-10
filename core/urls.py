00da5447adb3b93abc17c8f23cbb4ea34b599187
hawas123 <hawas@techflipp.com>
2023-12-10 20:42:17 +0200

    add tweeoy

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, send_message, create_contact
+from .views import home, get_user_tweets, get_facebook_posts, send_message, create_contact
 
 
 
 urlpatterns = [
     path('', home, name='home' ),
-    # path('twitter', get_user_tweets, name='twitter' ),
-    # path('facebook', get_facebook_posts, name='facebook' ),
+    path('twitter', get_user_tweets, name='twitter' ),
+    path('facebook', get_facebook_posts, name='facebook' ),
     path('message', send_message, name='message' ),
     path('create_contact', create_contact, name='create_contact' )
 
