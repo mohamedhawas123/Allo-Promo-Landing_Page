2f275d9e78e7a8aa39ea65a43cd1038a0a85fbcf
hawas123 <hawas@techflipp.com>
2023-12-10 20:39:19 +0200

    fix requirement

--- core/twitter.py
+++ core/twitter.py
@@ -1,8 +1,8 @@
-import tweepy
-from django.conf import settings
+# import tweepy
+# from django.conf import settings
 
 
-def twitter_api():
-    auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
-    auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
-    return tweepy.API(auth)
+# def twitter_api():
+#     auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+#     auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
+#     return tweepy.API(auth)
